e0090b2b6b1290a17ab80f4bc557de7fa4ff07e4
Catherine Jimenez <xcatherine.jimenez@gmail.com>
2020-04-30 18:36:50 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -36,7 +36,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,16 +55,10 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#   :languages => ["LISP", "C"]
-# }
-# return the entire updated hash
 
 	programmer_hash = 
  		{
@@ -81,7 +75,13 @@ def adding_matz
         :languages => ["C"]
       }
     }
+    
+    programmer_hash[:yukihiro_matsumoto] = {
+      :known_for => "Ruby",
+     :languages => ["LISP", "C"]
+     }
 
+  programmer_hash
 end
 
 def changing_alan
@@ -103,6 +103,10 @@ def changing_alan
         :languages => ["C"]
       }
     }
+    
+    programmer_hash[:alan_kay][:known_for] = "GUI"
+    
+    programmer_hash
 
 end
 
@@ -125,5 +129,8 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-
+  
+  programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+  
+  programmer_hash
 end
